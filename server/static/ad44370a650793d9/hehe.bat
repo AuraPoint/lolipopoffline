434289740e03daa5385a64a93caba08b2ee30b1e
RedBoi/SilverStudios <70915454+RedBoiOfficial@users.noreply.github.com>
2021-06-20 12:04:41 +0300

    working (i swear i did not make hehe.bat)

---
+++ server/static/ad44370a650793d9/hehe.bat
@@ -0,0 +1,11 @@
+title heheheheheheheheehheehehehehehehehhehehheheheheheheheheheheheeheheehehheheeheheheh
+:: Computer Hehe.bat
+:: heheheh
+:: lisnece Heehehe: mit
+
+echo heheheheheehe
+echo Its hehehe lolz
+echo:
+echo hehehe
+echo idk what to do hahaha
+echo Car go brrr
